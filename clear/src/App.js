d000f423f90d230185db4cea1253835f5d8bd43c
Anusha Kalbande <anushakalbande17@gmail.com>
2024-11-21 23:46:56 -0500

    Basic landing page and file upload

--- clear/src/App.js
+++ clear/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [fileName, setFileName] = useState('');
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setFileName(file.name);
+      console.log('File uploaded:', file);
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-container">
+
+      <header className="header">
+        <h1 className="main-heading">ClearSpeak</h1>
+        <h2 className="sub-heading">Making transcriptions accessible for people with stuttering</h2>
       </header>
+
+      <main className="main-content">
+      <section className="upload-section">
+        <label htmlFor="file-upload" className="upload-label">
+          Upload a file
+        </label>
+        <input
+          type="file"
+          id="file-upload"
+          className="file-input"
+          onChange={handleFileUpload}
+        />
+        {fileName && <p className="file-name">Uploaded File: {fileName}</p>}
+      </section>
+      </main>
+
+      <footer className="footer">
+        <p>&copy; 2024 ClearSpeak | All Rights Reserved</p>
+      </footer>
+      
     </div>
   );
 }
